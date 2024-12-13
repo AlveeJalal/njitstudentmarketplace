73dee667cfa0e9741cf48cf90e5b80ef7397d21a
ajalal <alveej520@gmail.com>
2024-12-13 03:48:45 +0000

    newdir

--- postgres/docker-entrypoint-initdb.d/database.sql
+++ postgres/docker-entrypoint-initdb.d/database.sql
@@ -1,8 +1,8 @@
-CREATE USER development WITH PASSWORD 'development';
+CREATE USER development WITH PASSWORD 'p@t3r$0n';
 CREATE DATABASE development OWNER development;
 GRANT ALL ON SCHEMA public TO development;
 GRANT ALL PRIVILEGES ON DATABASE development TO development;
-CREATE USER production WITH PASSWORD 'production';
+CREATE USER production WITH PASSWORD '@tl@nt1cc1ty';
 GRANT ALL ON SCHEMA public TO production;
 CREATE DATABASE production OWNER production;
 GRANT ALL PRIVILEGES ON DATABASE production TO production;
